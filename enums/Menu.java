edc71c8a4e756b42f01ac5f39fa5aeb7ad1a1e03
sanyarrashidi <sanyar.rashidi1@gmail.com>
2025-04-12 19:44:54 +0330

    updated some models

--- enums/Menu.java
+++ enums/Menu.java
@@ -1,4 +1,20 @@
 package enums;
 
+import views.*;
+
 public enum Menu {
+    GameMenu(new GameMenu()),
+    InventoryMenu(new InventoryMenu()),
+    LoginMenu(new LoginMenu()),
+    MainMenu(new MainMenu()),
+    ProfileMenu(new ProfileMenu()),
+    SignUpMenu(new SignUpMenu()),
+    StoreMenu(new StoreMenu()),
+    ExitMenu(new ExitMenu());
+
+    private final AppMenu menu;
+
+    Menu(AppMenu menu) {
+        this.menu = menu;
+    }
 }
--- views/ExitMenu.java
+++ views/ExitMenu.java
@@ -1,4 +1,10 @@
 package views;
 
-public class ExitMenu {
+import java.util.Scanner;
+
+public class ExitMenu implements AppMenu {
+    @Override
+    public void checkInput(Scanner scanner) {
+
+    }
 }
