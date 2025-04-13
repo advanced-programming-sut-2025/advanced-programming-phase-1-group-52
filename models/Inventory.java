e8ac79ccd91416c165d1dec093e2ecae198508ca
RadinAjoodani <radin.ajoodani@gmail.com>
2025-04-13 12:20:03 +0330

    Tools added

--- models/Inventory.java
+++ models/Inventory.java
@@ -1,4 +1,11 @@
 package models;
 
+import enums.items.Backpacks;
+import models.item.Item;
+
+import java.util.ArrayList;
+
 public class Inventory {
+    private Backpacks backpack = Backpacks.PrimitiveBackpack;
+    private ArrayList<Item> items = new ArrayList<>();
 }
