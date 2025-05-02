6dbd5b6dd6e132ebecb81a929197206870b46c52
RadinAjoodani <radin.ajoodani@gmail.com>
2025-05-03 01:40:27 +0330

    date and time updated

--- models/Time.java
+++ models/Time.java
@@ -1,31 +1,45 @@
 package models;
 
 public class Time {
-    private int hour = 9;
+    public static final int DAY_START = 9;
+    public static final int DAY_END = 22;
+    public static final int ACTIVE_HOURS = DAY_END - DAY_START;
 
-    public Time() {
+    private int hour = DAY_START;
 
+    public int addHours(int hours) {
+        if (hours <= 0) return 0;
+
+        int elapsedHours = (this.hour - DAY_START) + hours;
+
+        int daysPassed = elapsedHours / ACTIVE_HOURS;
+        int remainingHours = elapsedHours % ACTIVE_HOURS;
+        this.hour = DAY_START + remainingHours;
+
+        if (remainingHours == 0 && hours > 0) {
+            this.hour = DAY_END;
+        }
+
+        return daysPassed;
     }
 
-    public void hourPassed() {
-        this.hour++;
+    public void advanceHour() {
+        addHours(1);
     }
 
     public boolean isDayOver() {
-        if (this.hour == 22) {
-            this.dayPassed();
-            return true;
-        }
-
-        return false;
+        return this.hour >= DAY_END;
     }
 
-    private void dayPassed() {
-        this.hour = 9;
-    }
-
+    // Getters and setters
     public int hour() {
         return hour;
     }
 
+    public void setHour(int hour) {
+        if (hour < DAY_START || hour > DAY_END) {
+            throw new IllegalArgumentException("Hour must be between 9 and 22");
+        }
+        this.hour = hour;
+    }
 }
